d141fb65ceac22a0a9ada19410ea0af03942cd0e
SK1PP3R <56693392+SK1PP3R@users.noreply.github.com>
2023-02-28 23:03:31 +0100

    Update user.sh

--- user.sh
+++ user.sh
@@ -9,8 +9,8 @@ fi
 # Lesen Sie den Servernamen aus der Umgebungsvariable $SERVERNAME
 SERVERNAME=$SERVERNAME
 
-# Lesen Sie den SSL-Flag aus der Umgebungsvariable $SSL
-SSL=${SSL:-false}
+# Lesen Sie den SSL-Flag aus der Umgebungsvariable $SSL_ENABLED
+SSL_ENABLED=${SSL_ENABLED:-false}
 
 # Lesen Sie den Indexes-Flag aus der Umgebungsvariable $INDEXES
 INDEXES=${INDEXES:-true}
@@ -38,7 +38,7 @@ else
 fi
 
 # Konfigurieren Sie die SSL-Zertifikats- und Schlüsseldateien basierend auf der Umgebungsvariable $SSL_PATH
-if [[ "$SSL" == true ]]; then
+if [[ "$SSL_ENABLED" == true ]]; then
   SSL_CERT_FILE="$SSL_PATH/$SERVERNAME.cert.pem"
   SSL_KEY_FILE="$SSL_PATH/$SERVERNAME.privkey.pem"
 else
@@ -47,7 +47,7 @@ else
 fi
 
 # Erstellen Sie einen VirtualHost mit Port 80 und Weiterleitung auf HTTPS, wenn SSL aktiviert ist
-if [[ "$SSL" == true ]]; then
+if [[ "$SSL_ENABLED" == true ]]; then
   cat <<EOF > "$CONFIG_FILE"
 <IfModule mod_ssl.c>
 <VirtualHost *:80>
